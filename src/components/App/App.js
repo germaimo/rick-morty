07dd371adb6e0a4cf01f6a444d74a41e8bcbacb1
batimovil <german.figueroa@ort.edu.ar>
2022-06-05 19:49:00 -0300

    chequeando useCallback, antes de el recibia por consola multiples renders y con este uso tambien tengo el mismo problema

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import "bulma/css/bulma.min.css";
 import axios from 'axios';
 import Card from '../Card/Card';
@@ -6,38 +6,31 @@ import Card from '../Card/Card';
 const App = () => {
 
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-  useEffect( ()=>{
-    const getData = async () => {
-      try{
-        setLoading(true);
-        const response =  await axios.get(`https://rickandmortyapi.com/api/character`);
-        setData(response.data);
-      }catch(error){
-        console.log(error);
-      }
+  const getData = useCallback(async () => {
+
+    try {
+      const response = await axios.get(`https://rickandmortyapi.com/api/character`);
+      setData(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
     }
-    getData();
-  },[]);
 
+  }, []);
+
+  useEffect(() => { getData(); }, [getData]);
 
   return (
-    <div className="App">
-              <div className="columns">
-  <div className="column">
-    <Card/>
-  </div>
-  <div className="column">
-    Second column
-  </div>
-  <div className="column">
-    Third column
-  </div>
-  <div className="column">
-    Fourth column
-  </div>
-</div>
+    <div className="container">
+      {loading ? (
+        <p>Loading..</p>
+      ) : (
+        <div className='columns is-desktop is-multiline'>
+          {console.log(data)}
+        </div>
+      )}
     </div>
   );
 }
